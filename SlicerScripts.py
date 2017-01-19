8f1f87af8ddd1467977db5d4f8e63c93f7bad0d5
Hannah Wilkinson <1hw7@queensu.ca>
2017-01-19 09:21:38 -0500

    Update SlicerScripts.py

--- SlicerScripts.py
+++ SlicerScripts.py
@@ -1,7 +1,7 @@
-
+#creates a button in slicer and prints button clicked in python interactor
 def callback():
      print('button clicked')
 def main():
-    b = qt.QPushButton('Toggle')
-    b.connect('clicked()', callback)
-    b.show
+    button = qt.QPushButton('Click')
+    button.connect('clicked()', callback)
+    button.show
