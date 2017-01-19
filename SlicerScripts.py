3ad06bc64b016c88f2069c533202a5c3773e3c54
Hannah Wilkinson <1hw7@queensu.ca>
2017-01-19 08:40:49 -0500

    Update SlicerScripts.py

--- SlicerScripts.py
+++ SlicerScripts.py
@@ -1,7 +1,7 @@
 
-def toggle():
+def callback():
      print('button clicked')
 def main():
     b = qt.QPushButton('Toggle')
-    b.connect('clicked()', toggle)
+    b.connect('clicked()', callback)
     b.show
