2b48e64aed59a12051e324fb2c3bf1f511ec1cb1
Hannah Wilkinson <1hw7@queensu.ca>
2017-01-22 11:26:26 -0500

    Update SlicerScripts.py

--- SlicerScripts.py
+++ SlicerScripts.py
@@ -1,7 +1,22 @@
 #creates a button in slicer and prints button clicked in python interactor
 def callback():
      print('button clicked')
+     
+def nodeTransform():
+# Create transform node & translate
+    node = slicer.vtkMRMLLinearTransformNode()
+    node.SetName('PreModelToRas')
+    slicer.mrmlScene.AddNode(preModelToRas)
+    nodeToTranslate = vtk.vtkTransform()
+    nodeToTranslate.PreMultiply() 
+    nodeToTranslate.Translate(0, 100, 0)
+    nodeToTranslate.Update()
+    node.SetAndObserveTransformToParent(nodeToTranslate)
+
 def main():
     button = qt.QPushButton('Click')
     button.connect('clicked()', callback)
     button.show
+    nodeTransform()
+
+
